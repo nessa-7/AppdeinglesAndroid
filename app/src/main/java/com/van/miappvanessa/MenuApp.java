81a3daa9c7a986e66e1d8cbb9cabffe9d6542703
nessa <yisel7948@gmail.com>
2025-11-14 11:41:50 -0500

    check en menu

--- app/src/main/java/com/van/miappvanessa/MenuApp.java
+++ app/src/main/java/com/van/miappvanessa/MenuApp.java
@@ -3,20 +3,104 @@ package com.van.miappvanessa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MenuApp extends AppCompatActivity {
 
+    CheckBox chApr1, chApr2, chApr3, chApr4;
+    Button btnColores, btnAnimales, btnFrutas, btnNumeros;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_app);
 
+        chApr1 = findViewById(R.id.chxAprendido1);
+        chApr2 = findViewById(R.id.chxAprendido2);
+        chApr3 = findViewById(R.id.chxAprendido3);
+        chApr4 = findViewById(R.id.chxAprendido4);
+
+        btnColores = findViewById(R.id.btnColores);
+        btnAnimales = findViewById(R.id.btnAnimales);
+        btnFrutas = findViewById(R.id.btnFrutas);
+        btnNumeros = findViewById(R.id.btnNumeros);
+
+
+        chApr1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton compoundButton, boolean b) {
+                if(b){
+                    btnColores.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.blue)
+                    );
+                }
+                else {
+                    btnColores.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.blue2)
+                    );
+                }
+            }
+        });
+
+        chApr2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton compoundButton, boolean b) {
+                if(b){
+                    btnAnimales.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.purple)
+                    );
+                }
+                else {
+                    btnAnimales.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.purple2)
+                    );
+                }
+            }
+        });
+
+        chApr3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton compoundButton, boolean b) {
+                if(b){
+                    btnFrutas.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.green)
+                    );
+                }
+                else {
+                    btnFrutas.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.green2)
+                    );
+                }
+            }
+        });
+
+        chApr4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton compoundButton, boolean b) {
+                if(b){
+                    btnNumeros.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.pink)
+                    );
+                }
+                else {
+                    btnNumeros.setBackgroundTintList(
+                            ContextCompat.getColorStateList(MenuApp.this, R.color.pink2)
+                    );
+                }
+            }
+        });
     }
     public void irColores(View view){
         Intent colores = new Intent(MenuApp.this,Colores.class);
@@ -42,4 +126,6 @@ public class MenuApp extends AppCompatActivity {
         Intent volver = new Intent(MenuApp.this,Opciones.class);
         startActivity(volver);
     }
+
+
 }
